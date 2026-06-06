bea4db375a2ae20a5d0e97d4949bcc4016701b27
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2026-06-06 21:58:15 +0700

    fix: Home video load

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,10 +5,10 @@
       muted
       loop
       playsinline
+      preload="auto"
       class="w-full object-cover h-screen"
     >
-      <!-- <source src="https://res.cloudinary.com/dlydxsbfj/video/upload/v1773929561/home_video_1_cd5zsm.mp4" type="video/mp4" /> -->
-      <source src="/home_video.mp4" type="video/mp4"/>
+      <source src="https://res.cloudinary.com/dlydxsbfj/video/upload/f_auto/v1780757032/home_video_mobile_plq1he.mp4" type="video/mp4" />
     </video>
   </div>
 </template>
