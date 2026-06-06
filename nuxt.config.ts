2a7051246d26cc04c52bd858e9f1e671f7da7a78
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2026-06-06 15:09:30 +0700

    fix: deploying cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,3 @@
-// @ts-ignore
-import fs from 'node:fs'
-// @ts-ignore
-import path from 'node:path'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -14,18 +9,6 @@ export default defineNuxtConfig({
     }
   },
   nitro: {
-    preset: 'cloudflare-pages'
-  },
-
-  // Tambahkan hook ini untuk menghapus file _redirects sebelum di-deploy
-  hooks: {
-    'nitro:build:public-assets': (nitro) => {
-      const redirectsPath = path.join(nitro.options.output.publicDir, '_redirects')
-      
-      if (fs.existsSync(redirectsPath)) {
-        fs.unlinkSync(redirectsPath)
-        console.log('Successfully removed _redirects to prevent Cloudflare deployment loop.')
-      }
-    }
+    preset: 'cloudflare'
   }
 })
