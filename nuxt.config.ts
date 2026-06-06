7ebfdacd5addfbb847d870a1f0457054425b57b6
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2026-06-06 14:48:05 +0700

    fix: memperbaiki konfigurasi cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
+// @ts-ignore
+import fs from 'node:fs'
+// @ts-ignore
+import path from 'node:path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -6,6 +11,21 @@ export default defineNuxtConfig({
   router: {
     options: {
       scrollBehaviorType: 'smooth',
+    }
+  },
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
+
+  // Tambahkan hook ini untuk menghapus file _redirects sebelum di-deploy
+  hooks: {
+    'nitro:build:public-assets': (nitro) => {
+      const redirectsPath = path.join(nitro.options.output.publicDir, '_redirects')
+      
+      if (fs.existsSync(redirectsPath)) {
+        fs.unlinkSync(redirectsPath)
+        console.log('Successfully removed _redirects to prevent Cloudflare deployment loop.')
+      }
     }
   }
 })
