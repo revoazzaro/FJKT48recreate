549ca8e8c6aea3dadef28e0d1f0214cdb2833595
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2026-07-17 23:00:29 +0700

    fix: fixing fetch idn live

--- server/api/idn-live.ts
+++ server/api/idn-live.ts
@@ -4,39 +4,68 @@ export default defineEventHandler(async (event) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
       },
       body: {
-        query: 'query SearchLivestream { searchLivestream(query: "", limit: 100) { next_cursor result { slug title image_url view_count playback_url room_identifier status live_at end_at scheduled_at gift_icon_url category { name slug } creator { uuid username name avatar bio_description following_count follower_count is_follow } } }}'
-      }
+        operationName: "GetLivestream",
+        query: `
+          query GetLivestream($category: String, $page: Int){
+            getLivestreams(category: $category, page: $page){
+              slug
+              title
+              image_url
+              view_count
+              playback_url
+              room_identifier
+              status
+              scheduled_at
+              live_at
+              live_type
+              category {
+                name
+                slug
+              }
+              creator{
+                name
+                username
+                uuid
+                avatar
+              }
+            }
+          }
+        `,
+        variables: {
+          page: 1,
+          category: "idol",
+        },
+      },
     });
 
-    const data = response?.data?.searchLivestream?.result;
+    const data = response?.data?.getLivestreams;
 
-    if (!data || data.length === 0) {
+    if (!data || data.length === null) {
       return { status: true, live_count: 0, data: [] };
     }
 
     const jkt48Lives = data
       .filter((i: any) => {
-        const username = i.creator?.username?.toLowerCase() || '';
-        const name = i.creator?.name?.toLowerCase() || '';
-        return username.includes('jkt48') || name.includes('jkt48');
+        const username = i.creator?.username?.toLowerCase() || "";
+        const name = i.creator?.name?.toLowerCase() || "";
+        return username.includes("jkt48") || name.includes("jkt48");
       })
       .map((i: any) => {
         return {
-          platform: 'idn',
+          platform: "idn",
           user: {
             id: i.creator?.uuid,
             name: i.creator?.name,
             username: i.creator?.username,
-            avatar: i.creator?.avatar,
+            avatar: i.creator?.avatar || "",
           },
           image: i.image_url,
           stream_url: i.playback_url,
-          title: i.title,
+          title: i.title ? i.title.trim() : "-",
           slug: i.slug,
           view_count: i.view_count,
           live_at: i.live_at ? new Date(i.live_at).toISOString() : null,
@@ -46,14 +75,13 @@ export default defineEventHandler(async (event) => {
     return {
       status: true,
       live_count: jkt48Lives.length,
-      data: jkt48Lives
+      data: jkt48Lives,
     };
-
   } catch (error: any) {
     console.error("Detail Error Terjadi di Server:", {
       message: error.message,
       statusCode: error.statusCode || error.status,
-      responseData: error.data
+      responseData: error.data,
     });
 
     return createError({
